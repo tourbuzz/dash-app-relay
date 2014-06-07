524e174f043a6d2d8cbf471282390a6bd88bf30d
Alan Pinstein <apinstein@mac.com>
2014-06-06 23:24:18 -0400

    Working PoC.

---
+++ dash-server.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+
+$socket = new React\Socket\Server($loop);
+$socket->on('connection', function ($conn) {
+    $conn->write("Hello there!\n");
+    $conn->write("Welcome to this amazing server!\n");
+    $conn->write("Send over any string in format: keys=php&query=array and I'll open it in Dash\n");
+
+    $conn->on('data', function ($data) use ($conn) {
+        $docRequest = array();
+        parse_str($data, $docRequest);
+
+        $conn->write("Searching {$docRequest['keys']} for {$docRequest['query']}\n");
+        $dashUrl = escapeshellarg("dash-plugin://{$data}");
+        `open dash-plugin://{$dashUrl}`;
+    });
+});
+$socket->listen(1337);
+
+print "Starting Dash documentation lookup server..." . PHP_EOL;
+$loop->run();
+
